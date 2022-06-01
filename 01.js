c862025bb120d0024cbddc1f9ae1c7ad29181d8a
nikhil <nikhilmane187@gmail.com>
2022-06-01 22:22:45 +0530

    firsst commit

---
+++ 01.js
@@ -0,0 +1,31 @@
+
+//mkn
+// console.log(`hello world`);
+// bjbj
+// let firstName = "nikhil";
+
+// console.log(firstName);
+
+// console.log(firstName.length);
+// console.log(firstName[firstName.length-1]);
+
+// firstName= firstName.trim();
+// console.log(firstName.length);
+
+
+// firstName = firstName.toUpperCase();---NIKHIL
+// firstName = firstName.toLowerCase();--- nikhil
+
+// let newString = firstName.slice(0,4);
+// console.log(newString);---nikh
+
+let age =22;
+let firstName = "nikhil";
+
+// console.log(typeof firstName);--- String
+// console.log(typeof age);---Number
+
+// console.log(typeof (age+ ""));---String
+
+
+
